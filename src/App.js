ca3f6cf9c1fffdb8789ef9753f9abf5de6e3a6d7
Eric Lusk-Jopson <eluskjopson@gmail.com>
2023-03-15 23:12:48 -0700

    planning on how to get info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 function App() {
+  /*
+  // search type release and render results
+  // select result and submit get request for release by selected id
+  // if not available, alert user and suggest picking a different release from the results
+  // if available, display release information
+  // display credited artists
+  // get request all artist releases
+  // add releases ids to set
+  // get releases
+  // get credited artists
+  // calculate proportion of original artists
+  // return relases sorted in orer of decreasing proportion
+  */
+
   return <div className="App">This will be an excellent application</div>;
 }
 
