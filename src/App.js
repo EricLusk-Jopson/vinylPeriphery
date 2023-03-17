0963417ce9c8ab14115befc7495ff209813f5bfa
Eric Lusk-Jopson <eluskjopson@gmail.com>
2023-03-17 02:30:58 -0700

    toggle for excluding the original artist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+import { useState, useEffect, React } from "react";
+
 function App() {
-  /*
-  // search type release and render results
-  // select result and submit get request for release by selected id
-  // if not available, alert user and suggest picking a different release from the results
-  // if available, display release information
-  // display credited artists
-  // get request all artist releases
-  // add releases ids to set
-  // get releases
-  // get credited artists
-  // calculate proportion of original artists
-  // return relases sorted in orer of decreasing proportion
-  */
+  const [results, setResults] = useState([]);
+  const [seedRelease, setSeedRelease] = useState({});
+  const [displayResults, setDisplayResults] = useState([]);
+  const [excludeArtist, setExcludeArtist] = useState(true);
+  const [formData, setFormData] = useState({
+    band: "",
+    album: "",
+  });
+  const { band, album } = formData;
 
-  const album = "nevermind";
-  const band = "nirvana";
-  const searchURL =
-    "https://api.discogs.com/database/search?release_title=nevermind&artist=nirvana&per_page=3&page=1&type=release&sort=year&sort_order=asc&key=owJjvljKmrcdSbXFVPTu&secret=wgJurrmQFbROAyrmByuLrZMRMhDznPaK";
-
-  const getInfo = async () => {
+  const getInfo = async (band, album) => {
     // return the search results
     console.log("searching for album");
     const searchResult = await fetch(
@@ -27,76 +20,125 @@ function App() {
 
     console.log(searchResult);
     console.log(searchResult.results[0].resource_url);
-
-    // look up the first result
-    console.log("lookup the first result");
-    const release = await fetch(searchResult.results[0].resource_url).then(
-      (res) => res.json()
-    );
+    let release;
+    // locate a release record with artist credits
+    for (let i = 0; i < 5; i++) {
+      const nextRelease = await fetch(
+        searchResult.results[i].resource_url
+      ).then((res) => res.json());
+      console.log(i, nextRelease);
+      if (nextRelease.extraartists && nextRelease.extraartists.length > 0) {
+        release = nextRelease;
+        break;
+      }
+      release = nextRelease;
+    }
     console.log(release);
     console.log(release.extraartists);
 
     const contributors = new Set();
+    const contributorInfo = new Map();
 
     // must look up artists and add their members to the list of contributors
     await Promise.all(
       release.artists.map(async (artist) => {
         contributors.add(artist.id);
+        contributorInfo.set(artist.id, {
+          name: artist.name,
+          role: artist.role,
+        });
         const artistInfo = await fetch(artist.resource_url).then((res) =>
           res.json()
         );
 
-        artistInfo.members.forEach((member) => contributors.add(member.id));
+        artistInfo.members?.forEach((member) => {
+          contributors.add(member.id);
+          contributorInfo.set(member.id, {
+            name: member.name,
+            role: "Band Member",
+          });
+        });
         console.log(artistInfo);
       })
     );
 
-    release.extraartists.forEach((extraArtist) =>
-      contributors.add(extraArtist.id)
-    );
+    release.extraartists.forEach((extraArtist) => {
+      if (
+        !extraArtist.role.includes("Management") &&
+        !extraArtist.role.includes("Photo") &&
+        !extraArtist.role.includes("Translated")
+      ) {
+        contributors.add(extraArtist.id);
+        contributorInfo.set(extraArtist.id, {
+          name: extraArtist.name,
+          role: extraArtist.role,
+        });
+      }
+    });
 
     console.log(contributors);
+    console.log(contributorInfo);
     console.log(contributors.values());
 
-    const constributorReleases = [];
+    const contributorReleases = [];
     await Promise.all(
       [...contributors.values()].map(async (artist_id) => {
         const releases = await fetch(
-          `https://api.discogs.com/artists/${artist_id}/releases`
+          `https://api.discogs.com/artists/${artist_id}/releases?page=1&per_page=100`
         ).then((res) => res.json());
-        constributorReleases.push(...releases.releases);
+        contributorReleases.push({
+          name: contributorInfo.get(artist_id).name,
+          role: contributorInfo.get(artist_id).role,
+          releases: releases.releases,
+        });
         console.log(releases);
-        console.log(constributorReleases);
+        console.log(contributorReleases);
+        console.log(contributorReleases);
       })
     );
 
-    console.log(constributorReleases);
+    console.log(contributorReleases);
     const releaseWeights = new Map();
-    constributorReleases.forEach((release) => {
-      console.log(
-        "id: ",
-        release.id,
-        ", inc: ",
-        releaseWeights.get(release.id)
-      );
-      // check if release has a main_release
-      if (releaseWeights.get(release.id)) {
-        const currentWeight = releaseWeights.get(release.id).weight;
-        releaseWeights.set(release.id, {
-          ...releaseWeights.get(release.id),
-          weight: currentWeight + 1,
-        });
-      } else {
-        const release_id = release.main_release
-          ? release.main_release
-          : release.id;
-        releaseWeights.set(release.id, {
-          mainRelease: release_id,
-          weight: 1,
-          artist: release.artist,
-          title: release.title,
-        });
-      }
+    contributorReleases.forEach((contributor) => {
+      contributor.releases.forEach((release) => {
+        console.log(
+          "id: ",
+          release.id,
+          ", inc: ",
+          releaseWeights.get(release.id)
+        );
+        // check if release has a main_release
+        const currentRelease = releaseWeights.get(release.id);
+        if (currentRelease) {
+          if (
+            !currentRelease.members.some(
+              (member) => member.name === contributor.name
+            )
+          ) {
+            const currentWeight = currentRelease.weight;
+            const currentMembers = [...currentRelease.members];
+            releaseWeights.set(release.id, {
+              ...currentRelease,
+              weight: currentWeight + 1,
+              members: [
+                ...currentMembers,
+                { name: contributor.name, role: contributor.role },
+              ],
+            });
+          }
+        } else {
+          const release_id = release.main_release
+            ? release.main_release
+            : release.id;
+          releaseWeights.set(release.id, {
+            mainRelease: release_id,
+            weight: 1,
+            artist: release.artist,
+            title: release.title,
+            members: [{ name: contributor.name, role: contributor.role }],
+          });
+        }
+      });
     });
 
     console.log(releaseWeights);
@@ -106,12 +148,76 @@ function App() {
       )
     );
     console.log(sortedResults);
+    console.log([...sortedResults.entries()]);
+    setSeedRelease(release);
+    setResults([...sortedResults.entries()]);
   };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const toggleArtistExclusion = (e) => {
+    e.preventDefault();
+    setExcludeArtist(!excludeArtist);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getInfo(band, album);
+  };
+
+  useEffect(() => {
+    const filteredResults = excludeArtist
+      ? results.filter((res) => res[1].artist !== seedRelease.artists_sort)
+      : results;
+    setDisplayResults(filteredResults);
+    console.log(excludeArtist);
+  }, [results, excludeArtist]);
 
   return (
     <div className="App">
       This will be an excellent application
-      <button onClick={getInfo}>get Info</button>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="band"
+          value={band}
+          placeholder="band name"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          name="album"
+          value={album}
+          placeholder="album name"
+          onChange={onChange}
+        />
+        <button>get Info</button>
+      </form>
+      <label>Exlude listings by the same artist</label>
+      <input
+        type="checkbox"
+        defaultChecked
+        checked={excludeArtist}
+        onChange={toggleArtistExclusion}
+      ></input>
+      <div>
+        {displayResults.map((result) => (
+          <>
+            <span></span>
+            <p>
+              {result[1].artist} - {result[1].title} featuring{" "}
+              {result[1].members
+                .map((member) => `${member.name} (${member.role})`)
+                .join(", ")}
+            </p>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
