ed8bd6ed23b5ca447b4a08a2e439f8fec3cbddec
Eric Lusk-Jopson <eluskjopson@gmail.com>
2023-03-18 16:55:00 -0700

    removed old getInfo method

--- src/App.js
+++ src/App.js
@@ -282,150 +282,6 @@ function App() {
     return output;
   };
 
-  const getInfo = async (band, album) => {
-    // return the search results
-    console.log("searching for album");
-    const searchResult = await fetch(
-      `https://api.discogs.com/database/search?release_title=${album}&artist=${band}&type=release&sort=year&sort_order=asc&key=owJjvljKmrcdSbXFVPTu&secret=wgJurrmQFbROAyrmByuLrZMRMhDznPaK`
-    ).then((res) => res.json());
-
-    console.log(searchResult);
-    console.log(searchResult.results[0].resource_url);
-    let release;
-    // locate a release record with artist credits
-    for (let i = 0; i < 5; i++) {
-      const nextRelease = await fetch(
-        searchResult.results[i].resource_url
-      ).then((res) => res.json());
-      console.log(i, nextRelease);
-      if (nextRelease.extraartists && nextRelease.extraartists.length > 0) {
-        release = nextRelease;
-        break;
-      }
-      release = nextRelease;
-    }
-    console.log(release);
-    console.log(release.extraartists);
-
-    const contributors = new Set();
-    const contributorInfo = new Map();
-
-    // must look up artists and add their members to the list of contributors
-    await Promise.all(
-      release.artists.map(async (artist) => {
-        contributors.add(artist.id);
-        contributorInfo.set(artist.id, {
-          name: artist.name,
-          role: artist.role,
-        });
-        const artistInfo = await fetch(artist.resource_url).then((res) =>
-          res.json()
-        );
-
-        artistInfo.members?.forEach((member) => {
-          contributors.add(member.id);
-          contributorInfo.set(member.id, {
-            name: member.name,
-            role: "Band Member",
-          });
-        });
-        console.log(artistInfo);
-      })
-    );
-
-    release.extraartists.forEach((extraArtist) => {
-      if (
-        !extraArtist.role.includes("Management") &&
-        !extraArtist.role.includes("Photo") &&
-        !extraArtist.role.includes("Translated") &&
-        !extraArtist.role.includes("Art") &&
-        !extraArtist.role.includes("Master")
-      ) {
-        contributors.add(extraArtist.id);
-        contributorInfo.set(extraArtist.id, {
-          name: extraArtist.name,
-          role: extraArtist.role,
-        });
-      }
-    });
-
-    console.log(contributors);
-    console.log(contributorInfo);
-    console.log(contributors.values());
-
-    const contributorReleases = [];
-    await Promise.all(
-      [...contributors.values()].map(async (artist_id) => {
-        const releases = await fetch(
-          `https://api.discogs.com/artists/${artist_id}/releases?page=1&per_page=100`
-        ).then((res) => res.json());
-        contributorReleases.push({
-          name: contributorInfo.get(artist_id).name,
-          role: contributorInfo.get(artist_id).role,
-          releases: releases.releases,
-        });
-        console.log(releases);
-        console.log(contributorReleases);
-        console.log(contributorReleases);
-      })
-    );
-
-    console.log(contributorReleases);
-    const releaseWeights = new Map();
-    contributorReleases.forEach((contributor) => {
-      contributor.releases.forEach((release) => {
-        console.log(
-          "id: ",
-          release.id,
-          ", inc: ",
-          releaseWeights.get(release.id)
-        );
-        // check if release has a main_release
-        const currentRelease = releaseWeights.get(release.id);
-        if (currentRelease) {
-          if (
-            !currentRelease.members.some(
-              (member) => member.name === contributor.name
-            )
-          ) {
-            const currentWeight = currentRelease.weight;
-            const currentMembers = [...currentRelease.members];
-            releaseWeights.set(release.id, {
-              ...currentRelease,
-              weight: currentWeight + 1,
-              members: [
-                ...currentMembers,
-                { name: contributor.name, role: contributor.role },
-              ],
-            });
-          }
-        } else {
-          const release_id = release.main_release
-            ? release.main_release
-            : release.id;
-          releaseWeights.set(release.id, {
-            mainRelease: release_id,
-            weight: 1,
-            artist: release.artist,
-            title: release.title,
-            members: [{ name: contributor.name, role: contributor.role }],
-          });
-        }
-      });
-    });
-
-    console.log(releaseWeights);
-    const sortedResults = new Map(
-      [...releaseWeights.entries()].sort(
-        (a, b) => b[1]["weight"] - a[1]["weight"]
-      )
-    );
-    console.log(sortedResults);
-    console.log([...sortedResults.entries()]);
-    // setSeedRelease(release);
-    // setResults([...sortedResults.entries()]);
-  };
-
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -436,11 +292,6 @@ function App() {
   const toggleArtistExclusion = (e) => {
     e.preventDefault();
     setExcludeArtist(!excludeArtist);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    getInfo(band, album);
   };
 
   const getBandReleases = async () => {
@@ -498,7 +349,7 @@ function App() {
   return (
     <div className="App">
       This will be an excellent application
-      <form onSubmit={onSubmit}>
+      <form>
         <input
           type="text"
           name="band"
@@ -513,7 +364,6 @@ function App() {
           placeholder="album name"
           onChange={onChange}
         />
-        <button>get Info</button>
       </form>
       <label>Exlude listings by the same artist</label>
       <input
@@ -547,19 +397,6 @@ function App() {
       >
         Load Next Page
       </button>
-      {/* <div>
-        {displayResults.map((result) => (
-          <>
-            <span></span>
-            <p>
-              {result[1].artist} - {result[1].title} featuring{" "}
-              {result[1].members
-                .map((member) => `${member.name} (${member.role})`)
-                .join(", ")}
-            </p>
-          </>
-        ))}
-      </div> */}
     </div>
   );
 }
